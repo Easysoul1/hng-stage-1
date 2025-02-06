4e8f4ee6d076f79f12fd4d19f924657f6d0d647c
Adedayo Ezekiel Adejumo <167407110+Easysoul1@users.noreply.github.com>
2025-02-06 01:12:43 +0100

    Correct some errors

--- stage1.js
+++ stage1.js
@@ -56,8 +56,8 @@ app.get("/api/classify-number", async (req, res) => {
     // Validate input
     if (!number || isNaN(number) || !Number.isInteger(Number(number))) {
         return res.status(400).json({
-            number: number,
-            error: true
+            error: true,
+            number: number
         });
     }
 
